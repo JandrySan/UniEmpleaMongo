d7454bfe146d50742b03334438b489f16df16fcf
JandrySan <94795287+JandrySan@users.noreply.github.com>
2026-01-19 21:59:18 -0500

    Simplify MongoDB connection setup
    Refactored MongoDB connection to simplify instantiation.

--- database/mongo_connection.py
+++ database/mongo_connection.py
@@ -3,15 +3,5 @@ from pymongo import MongoClient
 
 MONGO_URI = os.getenv("MONGO_URI")
 
-class MongoDB:
-    _instancia = None
-
-    def __new__(cls):
-        if cls._instancia is None:
-            cls._instancia = super().__new__(cls)
-            client = MongoClient(MONGO_URI)
-            cls._instancia.db = client["uniemplea"]  # nombre explícito
-        return cls._instancia
-
-
-
+client = MongoClient(MONGO_URI)
+db = client.get_database()  # o client["uniemplea"]
