bcc1767a1eb1723abfe3de4d1efc7b991379d4fe
JandrySan <94795287+JandrySan@users.noreply.github.com>
2026-01-19 22:22:42 -0500

    Refactor mongo_connection.py for error handling
    Change exception type for missing MONGO_URI and use get_database method.

--- database/mongo_connection.py
+++ database/mongo_connection.py
@@ -4,8 +4,9 @@ from pymongo import MongoClient
 MONGO_URI = os.getenv("MONGO_URI")
 
 if not MONGO_URI:
-    raise Exception("❌ MONGO_URI no está definida")
+    raise ValueError("MONGO_URI no está definida")
 
 client = MongoClient(MONGO_URI)
+db = client.get_database()
 
-db = client["uniemplea"]  # 👈 NOMBRE EXPLÍCITO DE LA BD
+
