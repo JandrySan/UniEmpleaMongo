5e5b3b4794361db3932687a54879751269f722a2
JandrySan <94795287+JandrySan@users.noreply.github.com>
2026-01-19 19:48:23 -0500

    Refactor MongoDB connection to use MONGO_URI
    Updated MongoDB connection to use environment variable for URI.

--- database/mongo_connection.py
+++ database/mongo_connection.py
@@ -1,3 +1,5 @@
+# database/mongodb.py (por ejemplo)
+import os
 from pymongo import MongoClient
 
 class MongoDB:
@@ -6,6 +8,13 @@ class MongoDB:
     def __new__(cls):
         if cls._instancia is None:
             cls._instancia = super().__new__(cls)
-            client = MongoClient("mongodb://localhost:27017/")
+
+            mongo_uri = os.getenv(
+                "MONGO_URI",
+                "mongodb://localhost:27017/"  # fallback local
+            )
+
+            client = MongoClient(mongo_uri)
             cls._instancia.db = client["uniemplea_db"]
+
         return cls._instancia
